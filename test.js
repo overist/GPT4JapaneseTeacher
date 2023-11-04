8ea511c2763ee3eb461f95c536618d2882241dcb
wj <wj@over.ist>
2023-11-04 13:34:19 +0900

    initical commit

---
+++ test.js
@@ -0,0 +1,21 @@
+// user
+// assistant
+// system
+
+import OpenAI from 'openai';
+require("dotenv").config();
+
+const openai = new OpenAI({
+  apiKey: 'My API Key', // defaults to process.env["OPENAI_API_KEY"]
+});
+
+async function main() {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'Say this is a test' }],
+    model: 'gpt-3.5-turbo',
+  });
+
+  console.log(chatCompletion.choices);
+}
+
+main();
